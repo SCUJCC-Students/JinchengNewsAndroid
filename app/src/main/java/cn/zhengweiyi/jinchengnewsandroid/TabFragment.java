3a84c4c03b7e5b7aa5ccfff62348e22de0862f86
郑维一 <weiyi@zhengweiyi.cn>
2018-05-22 15:02:58 +0800

    修改 阅读量变量名称
    删除 没有使用的导包

--- app/src/main/java/cn/zhengweiyi/jinchengnewsandroid/TabFragment.java
+++ app/src/main/java/cn/zhengweiyi/jinchengnewsandroid/TabFragment.java
@@ -9,7 +9,6 @@
 package cn.zhengweiyi.jinchengnewsandroid;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
