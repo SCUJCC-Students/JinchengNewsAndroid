b6294fbfa9d3571b71373b45fd1ccb69d685928b
郑维一 <weiyi@zhengweiyi.cn>
2018-05-23 17:26:54 +0800

    优化 NewsListAdapter代码结构
    删除 社团页(TabFragment2)测试文本

--- app/src/main/java/cn/zhengweiyi/jinchengnewsandroid/NewsListAdapter.java
+++ app/src/main/java/cn/zhengweiyi/jinchengnewsandroid/NewsListAdapter.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 public class NewsListAdapter extends ArrayAdapter<News> {
     private int resourecId;
-    public Context context;
 
     public NewsListAdapter(Context context, int resource, List<News> objects) {
         super( context, resource, objects);
@@ -28,7 +27,7 @@ public class NewsListAdapter extends ArrayAdapter<News> {
         /* return super.getView(position, convertView, parent); */
         News news = getItem( position);
         View view;
-        context =getContext();
+        Context context = getContext();
 
         if (news!=null) {
 
@@ -37,24 +36,27 @@ public class NewsListAdapter extends ArrayAdapter<News> {
             } else {
                 view = convertView;
             }
+
             //设置新闻标题
             TextView newTitleText = (TextView) view.findViewById(R.id.newsTitle);
             newTitleText.setText(news.getTitle());
+
             //设置新闻作者
             TextView newAuthorText = (TextView) view.findViewById(R.id.newsAuthor);
             newAuthorText.setText(news.getAuthor());
+
             //设置新闻浏览量
-            // TODO move getResources() to Activity
             TextView newViewsText = (TextView) view.findViewById(R.id.newsViews);
             String t = String.format(context.getString(R.string.news_views), news.getViews());
             newViewsText.setText(t);
+            
             //设置新闻图片
             ImageView newImageSrc = (ImageView) view.findViewById(R.id.newsImage);
 
             return view;
         }
 
-        //如果
+        //如果News为空，则返回空
     return null;
     }
 }
