ec9a2ea886385e38331cf8e03975064653f80fa8
郑维一 <weiyi@zhengweiyi.cn>
2018-05-29 15:30:25 +0800

    删除 多余代码
    完善 根据提示完善代码，消除警告

--- app/src/main/java/cn/zhengweiyi/jinchengnewsandroid/NewsListAdapter.java
+++ app/src/main/java/cn/zhengweiyi/jinchengnewsandroid/NewsListAdapter.java
@@ -9,6 +9,7 @@
 package cn.zhengweiyi.jinchengnewsandroid;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,13 +22,15 @@ import java.util.List;
 public class NewsListAdapter extends ArrayAdapter<News> {
     private int resourceId;
 
-    public NewsListAdapter(Context context, int resource, List<News> objects) {
+    NewsListAdapter(Context context, int resource, List<News> objects) {
         super( context, resource, objects);
         // TODO Auto-generated constructor stub
         resourceId = resource;
     }
+
+    @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
         // TODO Auto-generated method stub
         /* return super.getView(position, convertView, parent); */
         News news = getItem( position);
@@ -43,20 +46,20 @@ public class NewsListAdapter extends ArrayAdapter<News> {
             }
 
             //设置新闻标题
-            TextView newTitleText = (TextView) view.findViewById(R.id.newsTitle);
+            TextView newTitleText = view.findViewById(R.id.newsTitle);
             newTitleText.setText(news.getTitle());
 
             //设置新闻作者
-            TextView newAuthorText = (TextView) view.findViewById(R.id.newsAuthor);
+            TextView newAuthorText = view.findViewById(R.id.newsAuthor);
             newAuthorText.setText(news.getAuthor());
 
             //设置新闻浏览量
-            TextView newViewsText = (TextView) view.findViewById(R.id.newsViews);
+            TextView newViewsText = view.findViewById(R.id.newsReader);
             String t = String.format(context.getString(R.string.news_views), news.getViews());
             newViewsText.setText(t);
 
             //设置新闻图片
-            ImageView newImageSrc = (ImageView) view.findViewById(R.id.newsImage);
+            ImageView newImageSrc = view.findViewById(R.id.newsImage);
 
             return view;
         }
