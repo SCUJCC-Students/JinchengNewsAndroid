cdb95555d305337393760fc7a3d58ac4c8811a11
郑维一 <weiyi@zhengweiyi.cn>
2018-06-07 10:57:47 +0800

    修复 提示无法加载DevOpenHelper类的错误
    添加 sqlcipher依赖（GreenDAO 加密）
    删除 根据IDE提示删除MainActivity中多余的强制类型转换

--- app/src/main/java/cn/zhengweiyi/jinchengnewsandroid/MainActivity.java
+++ app/src/main/java/cn/zhengweiyi/jinchengnewsandroid/MainActivity.java
@@ -35,16 +35,16 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         /*初始化界面*/
@@ -66,13 +66,13 @@ public class MainActivity extends AppCompatActivity
 
     /*初始化界面*/
     private void initViews() {
-        this.pager = (ViewPager) findViewById(R.id.pager);
-        this.tab = (TabLayout) findViewById(R.id.tab);
+        this.pager =  findViewById(R.id.pager);
+        this.tab = findViewById(R.id.tab);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -137,7 +137,7 @@ public class MainActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
