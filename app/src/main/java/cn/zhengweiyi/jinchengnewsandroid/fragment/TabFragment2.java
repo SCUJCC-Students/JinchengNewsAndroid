713b74d951f335993b4e9adf3249d5c839660d41
郑维一 <weiyi@zhengweiyi.cn>
2018-06-19 21:18:23 +0800

    修改 尝试打开新活动

--- app/src/main/java/cn/zhengweiyi/jinchengnewsandroid/fragment/TabFragment2.java
+++ app/src/main/java/cn/zhengweiyi/jinchengnewsandroid/fragment/TabFragment2.java
@@ -9,6 +9,7 @@
 package cn.zhengweiyi.jinchengnewsandroid.fragment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -89,6 +90,8 @@ public class TabFragment2 extends Fragment implements AdapterView.OnItemClickLis
             contentActivity. actionStart(getActivity(), news.getTitle(), news.getContent());
         }
         */
+        Intent intent = new Intent(getActivity(), ScrollingContentActivity.class);
+        startActivity(intent);
         ScrollingContentActivity.actionStart(getActivity(), news.getTitle(), news.getContent(), news.getAuthor(), news.getViews(), news.getCategoryId());
     }
 }
